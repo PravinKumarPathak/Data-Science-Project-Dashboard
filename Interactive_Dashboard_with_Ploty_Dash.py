0738f59fd94d9a1d68ca7ed4296bef6a2cab16a6
PravinKumarPathak <140155995+PravinKumarPathak@users.noreply.github.com>
2024-03-11 18:13:56 +0530

    Update Interactive_Dashboard_with_Ploty_Dash.py

--- Interactive_Dashboard_with_Ploty_Dash.py
+++ Interactive_Dashboard_with_Ploty_Dash.py
@@ -1,8 +1,8 @@
 # Import required libraries
 import pandas as pd
 import dash
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import html
+from dash import dcc
 from dash.dependencies import Input, Output
 import plotly.express as px
 
@@ -24,7 +24,7 @@ dropdown_options=[{'label':'All Sites','value':'All'},
 
 # Create a dash application
 app = dash.Dash(__name__)
-
+server=app.server
 
 # Create an app layout
 app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
